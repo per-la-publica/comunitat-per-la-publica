da566cb033a1027f83f1dc466f1104c9775a3769
fv3rdugo <fv3rdugo@gmail.com>
2013-01-28 15:58:09 +0100

    Traduït

---
+++ mod/registrationterms/languages/ca.php
@@ -0,0 +1,6 @@
+<?php
+
+add_translation("ca", array(
+	'registrationterms:agree' => 'He llegit i estic d\'acord amb les <a target="_blank" href="%s">Condicions del Servei</a>',
+	'registrationterms:required' => "Caldrà que accepti abans les seves condicions del Servei",
+));
---
+++ mod/registrationterms/languages/es.php
@@ -0,0 +1,6 @@
+<?php
+
+add_translation("es", array(
+	'registrationterms:agree' => 'He leído y estoy de acuerdo con las <a target="_blank" href="%s">Condiciones del Servicio</a>',
+	'registrationterms:required' => "Es necesario aceptar primero las Condiciones del Servicio",
+));
