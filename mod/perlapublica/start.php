68fc35aceb167043257c25967a9be0395de1b99d
Oriol <oriol.teixido@gmail.com>
2013-02-11 22:27:33 +0100

    Realitzar les modificacions de la pàgina d'entrada al mòdul PerLaPublica

--- mod/perlapublica/start.php
+++ mod/perlapublica/start.php
@@ -1,48 +1,47 @@
 <?php
-    /**
+	/**
 	 * Per la Pública
 	 * 
 	 * @package perlapublica
 	 * @author Per la Pública Developers
 	 * @link http://www.perlapublica.org
-     *
-     * Select SQL to get all initial emails in database
-     * > SELECT u.username, s1.string, s2.string 
-     * > FROM users_entity u, metadata d, metastrings s1, metastrings s2 
-     * > WHERE u.guid = d.entity_guid AND d.name_id = s1.id AND s1.string = 'initial_email' AND d.value_id = s2.id;
-     *
-     * TODO: Pendent de utilitzar el sistema i18n de l'elgg.
-     */
+	 *
+ 	 * Select SQL to get all initial emails in database
+	 * > SELECT u.username, s1.string, s2.string 
+	 * > FROM users_entity u, metadata d, metastrings s1, metastrings s2 
+	 * > WHERE u.guid = d.entity_guid AND d.name_id = s1.id AND s1.string = 'initial_email' AND d.value_id = s2.id;
+	 *
+	 * TODO: Pendent de utilitzar el sistema i18n de l'elgg.
+	 */
 
 	/**
 	 * Initialization of plugin
 	 */
-    function perlapublica_init() {
-        elgg_register_plugin_hook_handler('register', 'user', 'perlapublica_save_initial_email');
-        elgg_register_plugin_hook_handler('registeruser:validate:email', 'all', 'perlapublica_filter_upc_address');
-        elgg_register_plugin_hook_handler('view', 'register/extend', 'perlapublica_filter_upc_address_information');
+    function xlp_init() {
+        elgg_register_plugin_hook_handler('register', 'user', 'xlp_save_initial_email');
+        elgg_register_plugin_hook_handler('registeruser:validate:email', 'all', 'xlp_filter_upc_address');
+        elgg_register_plugin_hook_handler('view', 'register/extend', 'xlp_filter_upc_address_information');
     }
 
-    elgg_register_event_handler('init', 'system', 'perlapublica_init');
-
+    elgg_register_event_handler('init', 'system', 'xlp_init');
 
     /**
      * Show information about email address from upc.edu
      */
-    function perlapublica_filter_upc_address_information($hook, $type, $returnvalue, $params) {
+    function xlp_filter_upc_address_information($hook, $type, $returnvalue, $params) {
         // we only want to alter when viewtype is "default"
         if ($params['viewtype'] !== 'default') {
             return $returnvalue;
         }
         $infoUpc = '';
-	    switch (get_current_language()) {
-            case 'es':
-			    $infoUpc = '<p>Con el fin de permitir el accesos sólo a miembros de la comunidad UPC, la dirección de correo tiene que pertenecer a la UPC.</p>'.
-                           '<p>Al finalizar el registro, recibirás, en unos minutos, un correo de confirmación en tu correo.</p>';
-                break;
-		    default:
-			    $infoUpc = '<p>Per tal de permetre l\'accés només a membres de la comunitat UPC, l\'adreça de correu ha de pertànyer a la UPC.</p>'.
-                           '<p>Al finalitzar el registre, rebràs, en uns minuts, un email de confirmació al teu correu.</p>';
+				switch (get_current_language()) {
+					case 'es':
+						$infoUpc = '<p>Con el fin de permitir el accesos sólo a miembros de la comunidad UPC, la dirección de correo tiene que pertenecer a la UPC.</p>'.
+						           '<p>Al finalizar el registro, recibirás, en unos minutos, un correo de confirmación en tu correo.</p>';
+						break;
+					default:
+						$infoUpc = '<p>Per tal de permetre l\'accés només a membres de la comunitat UPC, l\'adreça de correu ha de pertànyer a la UPC.</p>'.
+					             '<p>Al finalitzar el registre, rebràs, en uns minuts, un email de confirmació al teu correu.</p>';
 	    }
         return '<div class="elgg-message elgg-state-notice" style="margin-top:1em">'.$infoUpc.'</div>'.$returnvalue;
       
@@ -51,14 +50,14 @@
     /**
      * Email address from upc.edu
      */
-    function perlapublica_filter_upc_address($hook, $type, $returnvalue, $params) { 
+    function xlp_filter_upc_address($hook, $type, $returnvalue, $params) { 
         return $returnvalue && preg_match('/(@|\.)upc\.edu$/i', $params['email']);
     }
 
     /**
      * Handle registration and save initial email in $user->initial_email attribute.
      */
-    function perlapublica_save_initial_email($hook, $type, $value, $params) {
+    function xlp_save_initial_email($hook, $type, $value, $params) {
 	    $user = elgg_extract('user', $params);
 
 	    // no clue what's going on, so don't react.
