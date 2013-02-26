150a48a3f4659588257a2dd5da63ea3177c22c2e
Oriol <oriol.teixido@gmail.com>
2013-02-26 23:36:03 +0100

    - Ordenar els missatges dels grups de discussió del més nou al més antic. - Posar el formulari de resposta al principi de les respostes enlloc del final

---
+++ mod/bright-themeXLP/views/default/discussion/replies.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * List replies with optional add form
+ *
+ * @uses $vars['entity']        ElggEntity
+ * @uses $vars['show_add_form'] Display add form or not
+ */
+
+$show_add_form = elgg_extract('show_add_form', $vars, true);
+
+echo '<div id="group-replies" class="mtl">';
+
+$options = array(
+	'guid' => $vars['entity']->getGUID(),
+	'annotation_name' => 'group_topic_post',
+	'order_by' => 'time_created DESC',
+);
+
+$html = elgg_list_annotations($options);
+if ($html) {
+	if ($show_add_form) {
+		$form_vars = array('class' => 'mtm');
+		echo elgg_view_form('discussion/reply/save', $form_vars, $vars);
+	}
+	echo '<h3 class="mtm">' . elgg_echo('group:replies') . '</h3>';
+	echo $html;
+}
+else {
+	if ($show_add_form) {
+		$form_vars = array('class' => 'mtm');
+		echo elgg_view_form('discussion/reply/save', $form_vars, $vars);
+	}
+}
+
+echo '</div>';
