c8e9c0a88547168a20f2e006a56982d315bd6261
fv3rdugo <fv3rdugo@gmail.com>
2013-02-26 21:28:18 +0100

    Modified catalan translation

--- mod/member_selfdelete/languages/ca.php
+++ mod/member_selfdelete/languages/ca.php
@@ -1,52 +1,52 @@
 <?php
 
 $catalan = array(
-  'member_selfdelete' => "Auto Eliminaci&oacute; del Compte",
+  'member_selfdelete' => "Auto eliminaci&oacute; del compte",
   'member_selfdelete:account:delete' => "Eliminar compte",
   'member_selfdelete:invalid:confirmation' => "Confirmaci&oacute; no v&agrave;lida - &Eacute;s necess&agrave;ri introduir la paraula %s a la casella de confirmaci&oacute; per eliminar el teu compte.",
   'member_selfdelete:DELETE' => "ELIMINAR",
-  'member_selfdelete:delete:account' => "Elimina el Meu Compte",
+  'member_selfdelete:delete:account' => "Elimina el meu compte",
   'member_selfdelete:deleted' => "El teu compte ha estat esborrat",
   'member_selfdelete:explain:anonymize' => "Desactivar permanentment el teu compte, eliminar tota la informaci&oacute; del perfil, fotos, i relacions per&ograve; deixar&agrave; els continguts/comentaris p&uacute;ublics accessibles.  No es pot desfer.",
-  'member_selfdelete:explain:ban' => "Desactiva el teu compte, ja no &eacute;s podr&agrave; connectar o publicar nous continguts.  Els continguts/comentaris publics existent restaran visibles, encara que el perfil i la imatge no.  L'administrador pot re-activar el seu compte si ho desitja.",
+  'member_selfdelete:explain:ban' => "Desactiva el teu compte, ja no &eacute;s podr&agrave; connectar o publicar nous continguts.  Els continguts/comentaris p&uacute;blics existent restaran visibles, encara que el perfil i la imatge no.  L'administrador pot re-activar el seu compte si ho desitja.",
   'member_selfdelete:explain:choose' => "Seleccioneu com quedar&agrave; el vostre compte:",
   'member_selfdelete:explain:delete' => "Eliminar permanentment el teu compte i tot el contingut/comentaris que et pertanyen.",
-  'member_selfdelete:feedback:thanks' => "Es trists veure't marxar, per&ograve; gr&agrave;cies per deixar-nos un comentari.",
-  'member_selfdelete:inactive:user' => "Usuari Inactiu",
+  'member_selfdelete:feedback:thanks' => "&Eacute;s trist veure't marxar, per&ograve; gr&agrave;cies per deixar-nos un comentari.",
+  'member_selfdelete:inactive:user' => "Usuari inactiu",
   'member_selfdelete:label:reason' => "Opcional: Si us plau, explica'ns el motiu pel qual et dones de baixa.",
-  'member_selfdelete:label:confirmation' => "Per confirmar que tens la intenciÃ³ d'elimin el teu compte escriula paraula \"%s\" a continuac&oacute;",
+  'member_selfdelete:label:confirmation' => "Per confirmar que tens la intenció d'eliminar el teu compte escriula paraula \"%s\" a continuac&oacute;",
   'member_selfdelete:option:anonymize' => "Fer an&ograve;nim",
   'member_selfdelete:option:ban' => "Expulsar",
   'member_selfdelete:option:choose' => "Triar",
   'member_selfdelete:option:delete' => "Eliminar",
   'member_selfdelete:option:no' => "No",
-  'member_selfdelete:option:yes' => "Si", 
-  'member_selfdelete:options:explanation:ban' =>"All of the users content will remain accessible with existing permissions.  The display
-  name remains the same, the profile picture is reset to default.  The profile is visible, but will
-  display the fact that the account is banned.  They will not be able to log back in, though an administrator
-  can unban the account.",
+  'member_selfdelete:option:yes' => "S&iacute;", 
+  'member_selfdelete:options:explanation:ban' =>"Tot el contingut de l'usuari quedarà accessible amb els permisos existents. 
+El nom per mostrar quedarà el mateix, la imatge del perfil es reinicia a l'establerta per defecte. El perfil es visible, 
+però mostrarà el fet que el compte ha estat bloquejat. No seran capaços d'iniciar sessió, encara que un administrador pot desbloquejar el compte.",
 
   'member_selfdelete:options:explanation:delete' => "Tots els continguts/comentaris/informacions/etc de l'usuari/a seran eliminats definitivament.  El seu compte, ser&agrave; eliminat.  No es pot desfer.",
 
-  'member_selfdelete:options:explanation:anonymize' => "[Per traduir]All of the users content/comments will remain accessible with existing permissions.  All of their
-  relationships will be deleted (eg. no more friends).  All of their collections will be deleted.  All of their profile information and any other
-  identifiable information will be deleted.  The profile picture will be reset to default, display name will be reset to 'Inactive User',
-  email and password will be reset - they will be unable to log in, and won't receive site notifications.  The only information remaining
-  on the system will be the username - preserved only for correct links to content.  These changes are permanent and cannot be undone.",
+  'member_selfdelete:options:explanation:anonymize' => "Tot el contingut/comentaris de l'usuari quedar&agrave; accessible amb els permisos existents. 
+Totes les seves relacions seran eliminades (p.ex. no m&eacute;s amics). Totes les seves col&middot;leccions seran eliminades. Tota la informaci&oacute; del perfil i 
+qualsevol altre informaci&oacute; identificable ser&agrave; eliminada. La imatge del perfil es reiniciar&agrave; a l'establerta per defecte, el nom per mostrar 
+quedar&agrave; el mateix es reiniciar&agrave; a 'Usuari inactiu', correu electr&ograve;nic i contrasenya seran reiniciats -no seran capa&ccedil;os d'iniciar 
+sessi&oacute;, i no rebran notificacions del lloc. L'&uacute;nica informaci&oacute; que quedar&agrave; en el sistema ser&agrave; el nom d'usuari -preservat 
+nom&eacute;s per poder enlla&ccedil;ar correctament el contingut. Aquests canvis s&oacute;n permanents i no poden ser desfets.",
 
   'member_selfdelete:options:explanation:choose' => "Permet a l'usuari triar entre les opcions anteriors a la vegada que cancel&middot;la el seu compte.",
 
-  'member_selfdelete:options:explanation:prefix' => "These are the options for how the user can delete their account.",
+  'member_selfdelete:options:explanation:prefix' => "Aquestes s&oacute;n  les opcions que t&eacute; l'usuari per eliminar el seu compte.",
 
   'member_selfdelete:profile_view' => "Usuaris inactius que no tenen perfil",
-  'member_selfdelete:reasons:list' => "Comentari d'usuari d'Auto Eliminaci&oacute;",
+  'member_selfdelete:reasons:list' => "Comentari d'usuari d'auto eliminaci&oacute;",
   'member_selfdelete:select:feedback' => "Seleccionar si &eagrave;s recollir&agrave; comentari:",
   'member_selfdelete:select:method' => "Seleccionar el m&egrave;tode d'eliminaci&oacute;:",
-  'member_selfdelete:self:banned' => "Usuari d'Auto Eliminaci&oacute;",
+  'member_selfdelete:self:banned' => "Usuari d'auto eliminaci&oacute;",
   'member_selfdelete:submit' => "Trametre",
 
   'admin:users:member_selfdelete/reasons' => "Opinions dels usuaris eliminats",
   'admin:users:member_selfdelete:reasons' => "Opinions dels usuaris eliminats",
 );
-					
+
 add_translation("ca",$catalan);
