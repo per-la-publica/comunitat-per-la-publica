239f3aed36d5923b49b6659ee3be5abd6e037323
Oriol <oriol.teixido@gmail.com>
2013-03-04 23:12:56 +0100

    Afegit literals necessaris als fitxers d'idioma

---
+++ mod/bright-themeXLP/languages/ca.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Likes catalan language file
+ */
+
+$catalan = array(
+	'profile:initial_email' => 'Correu UPC',
+);
+
+add_translation('ca', $catalan);
+ 
---
+++ mod/bright-themeXLP/languages/es.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Likes Spanish language file
+ */
+
+$spanish = array(
+	'profile:initial_email' => 'Correo UPC',
+);
+
+add_translation('es', $spanish);
