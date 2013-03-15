efd035d261cf21752b83bfb878c825ccec0a1f16
Oriol Teixidó <oriol.teixido@gmail.com>
2013-03-15 14:28:31 +0100

    Creació del mòdul xlp-contact

--- mod/xlp-contact/views/default/xlp-contact/contactform.php
+++ mod/xlp-contact/views/default/xlp-contact/contactform.php
@@ -1,40 +1,24 @@
 <?php 
-elgg_get_plugin_setting('email', 'xlp-contact'); 
-?>
-<?PHP
-/*
-    Contact Form from HTML Form Guide
-    This program is free software published under the
-    terms of the GNU Lesser General Public License.
-    See this page for more info:
-    http://www.html-form-guide.com/contact-form/creating-a-contact-form.html
-*/
 
 require_once("./include/fgcontactform.php");
 require_once("./include/simple-captcha.php");
-$email=elgg_get_plugin_setting('email','contact');
+$email = elgg_get_plugin_setting('email','xlp-contact');
 $formproc = new FGContactForm();
 $sim_captcha = new FGSimpleCaptcha('scaptcha');
 
 $formproc->EnableCaptcha($sim_captcha);
 
 //1. Add your email address here.
-//You can add more than one receipients.
- $formproc->AddRecipient($email); //<<---Put your email address here
+$formproc->AddRecipient($email);
 
-//2. For better security. Get a random tring from this link: http://tinyurl.com/randstr
-// and put it here
-$formproc->SetFormRandomKey('CnRrspl1FyEylUj');
+//2. For better security. Get a random tring from this link: http://tinyurl.com/randstr and put it here
+$formproc->SetFormRandomKey('iBuFNzOzwyRxBc5');
 
-
-if(isset($_POST['submitted']))
-{
-   if($formproc->ProcessForm())
-   {
+if(isset($_POST['submitted'])) {
+   if($formproc->ProcessForm()) {
         $formproc->RedirectToURL("thank-you.php");
    }
 }
-
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
@@ -43,27 +27,26 @@ if(isset($_POST['submitted']))
       <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
 </head>
 <body>
+<div class="elgg-main elgg-body">
 <!-- Form Code Start -->
 
 &nbsp;
 <form id='contactus' action='<?php echo $formproc->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
-<fieldset >
-<legend>Contact us</legend>
+<h2>Contact us</h2>
 
+<fieldset >
 <input type='hidden' name='submitted' id='submitted' value='1'/>
 <input type='hidden' name='<?php echo $formproc->GetFormIDInputName(); ?>' value='<?php echo $formproc->GetFormIDInputValue(); ?>'/>
 <input type='text'  class='spmhidip' name='<?php echo $formproc->GetSpamTrapInputName(); ?>' />
 
-<div class='short_explanation'>* required fields</div>
-
 <div><span class='error'><?php echo $formproc->GetErrorMessage(); ?></span></div>
 <div class='container'>
-    <label for='name' >Your Full Name*: </label><br/>
+    <label for='name' >Your Full Name: </label><br/>
     <input type='text' name='name' id='name' value='<?php echo $formproc->SafeDisplay('name') ?>' maxlength="50" /><br/>
     <span id='contactus_name_errorloc' class='error'></span>
 </div>
 <div class='container'>
-    <label for='email' >Email Address*:</label><br/>
+    <label for='email' >Email Address:</label><br/>
     <input type='text' name='email' id='email' value='<?php echo $formproc->SafeDisplay('email') ?>' maxlength="50" /><br/>
     <span id='contactus_email_errorloc' class='error'></span>
 </div>
@@ -73,7 +56,6 @@ if(isset($_POST['submitted']))
     <textarea rows="10" cols="50" name='message' id='message'><?php echo $formproc->SafeDisplay('message') ?></textarea>
 </div>
 <fieldset id='antispam'>
-<legend >Anti-spam question</legend>
 <span class='short_explanation'>(Please answer the simple question below. This to prevent spam bots from submitting this form)</span>
 <div class='container'>
     <label for='scaptcha' ><?php echo $sim_captcha->GetSimpleCaptcha(); ?></label>
@@ -83,7 +65,7 @@ if(isset($_POST['submitted']))
 </fieldset>
 
 <div class='container'>
-    <input type='submit' name='Submit' value='Submit' />
+    <input type='submit' name='Submit' value="Submit" class="elgg-button elgg-button-submit" />
 </div>
 
 </fieldset>
@@ -114,6 +96,7 @@ Uses the excellent form validation script from JavaScript-coder.com-->
 
 // ]]>
 </script>
+</div>
 </body>
 </html>
 
